c070d47b6b4ce5f36f6c9bd3662b2672cecd791b
manuel-0310 <manucastil0310@gmail.com>
2025-05-22 22:25:45 -0500

    Update settings.py

--- backend/piazzeta_backend/settings.py
+++ backend/piazzeta_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-kix4!v0f#j9ykyvo(zn_eq&rmwgo!yrumb&9e#fnes)kdpm@2#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
