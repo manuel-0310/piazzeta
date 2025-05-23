e74749dd55b9bd9e15f5c9c083fa61a4ca5f78f4
manuel-0310 <manucastil0310@gmail.com>
2025-05-22 22:15:34 -0500

    Dominio

--- backend/piazzeta_backend/settings.py
+++ backend/piazzeta_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kix4!v0f#j9ykyvo(zn_eq&rmwgo!yrumb&9e#fnes)kdpm@2#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'piazzeta.onrender.com').split(',')
 
 
 # Application definition
